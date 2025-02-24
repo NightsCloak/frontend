aaf027604a0d884d0404e7c8ac41c258fe0b7aac
ajnozari <aj@intract.com>
2025-02-24 13:50:57 -0500

    More Chronicle work

---
+++ src/screens/chronicles/ChroniclesDashboard.tsx
@@ -0,0 +1,21 @@
+import { Box, Paper, Typography } from '@mui/material';
+import ChroniclesTable from '@/screens/chronicles/ChroniclesTable';
+import { makeStyles } from 'tss-react/mui';
+
+const ChroniclesDashboard = () => {
+    const { classes } = useStyles();
+
+    return (
+        <Box className={classes.root} component={Paper}>
+            <Typography variant={'h3'}>Chronicles</Typography>
+            <ChroniclesTable />
+        </Box>
+    );
+};
+
+const useStyles = makeStyles()((theme) => ({
+    root: { display: 'flex', flexShrink: 1, flexDirection: 'column', padding: theme.spacing(2), minHeight: 400 },
+    header: { marginBottom: theme.spacing(2) },
+}));
+
+export default ChroniclesDashboard;
